d73504377596f4fa075d986cd2d15d65393c1d38
Dilunga SR <dilungasalumtz@gmail.com>
2023-03-28 11:21:01 +0300

    Add asyncId property

--- message.js
+++ message.js
@@ -1,9 +1,9 @@
 // Message  models the data to be sent to the server
 export class Message {
-    constructor(action, data, rec) {
-      this.action = action;
-      this.data = data;
-      this.rec = rec;
-    }
+  constructor(action, data, rec, asyncId) {
+    this.action = action;
+    this.data = data;
+    this.rec = rec;
+    this.asyncId = asyncId;
   }
-  
+}
